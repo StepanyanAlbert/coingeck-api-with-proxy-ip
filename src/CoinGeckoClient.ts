eba801c3041fa729dde482a2c414169efbb72998
Grizelda <grizeldamkrtchyan95@gmail.com>
2022-07-29 14:11:52 +0400

    changes

--- src/CoinGeckoClient.ts
+++ src/CoinGeckoClient.ts
@@ -180,7 +180,7 @@ valid values: true, false
    */
   public async coinMarket(input: {
     vs_currency: string,
-    ids: string,
+    ids?: string,
     category?: 'decentralized_finance_defi' | 'stablecoins',
     order?: 'market_cap_desc' | 'gecko_desc' | 'gecko_asc' | 'market_cap_asc' | 'market_cap_desc' | 'volume_asc' | 'volume_desc' | 'id_asc' | 'id_desc',
     per_page?: number
